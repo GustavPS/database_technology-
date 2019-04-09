43f611c350947627afddde6e80a04d6e92f52431
Gustav P Svensson <gustavps97@gmail.com>
2019-04-09 12:59:41 +0200

    gjort ER diagram

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,3 +1,8 @@
+/*
+3: Implement your extensions in the database by first creating tables, if any, then
+populating them with existing manager data, then adding/modifying foreign key
+constraints. Do you have to initialize the bonus attribute to a value? Why?
+*/
 CREATE TABLE jbmanager(
        id INT,	     
        bonus INT,
@@ -21,3 +26,14 @@ FOREIGN KEY (`manager`) REFERENCES `jbmanager`(`id`);
 ALTER TABLE jbemployee
 ADD CONSTRAINT `fk_emp_mgr`
 FOREIGN KEY (manager) REFERENCES jbmanager(id);
+
+
+/*
+4: All departments showed good sales figures last year! Give all current department
+managers $10,000 in bonus. This bonus is an addition to other possible bonuses
+they have.
+Hint: Not all managers are department managers. Update all managers that are
+referred in the jbdept relation.
+*/
+UPDATE jbmanager SET bonus = 10000
+WHERE id IN (SELECT DISTINCT manager FROM jbdept);
